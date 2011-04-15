f7ed28beb1d51af89c0a131f1dfa529d291641c0
bestarnes <bestarnes@bd6c6543-9d70-a1b8-c043-35d555d4dc2c>
2011-04-15 09:15:44 +0000

    Updated bonusBall code so that it compiles and works.

--- Code/BonusBot/util.h
+++ Code/BonusBot/util.h
@@ -3,26 +3,10 @@
 
 extern u08 pidStop;
 
-void pidDrive(s08 wallSpeed, s08 innerSpeed);
-void pidExec();
-void driveForward(s08 wallSpeed, s08 innerSpeed);
-void turnLeft();
-void turnRight();
 void stop();
-void scraperDown();
-void scraperUp();
-void feederOn();
-void feederOff();
 void haltRobot();
-void resetEncoders();
-void calibrate(int ticksPerSec, s08* wallMotorSpeed, s08* innerMotorSpeed);
-void compCollectFwd();
-void compCollectBack();
 void compDone();
-void hugWallForwards();
-void hugWallBackwards();
 void victoryDance();
-u16 convertToBatteryVoltage(u16 reading);
 
 void launcherSpeed(u08 speed);
 void launcherExec();
